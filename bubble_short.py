68d28b49c47d3ac87953a7049a9722e449c036cf
jahanRazh <jahanrazh@gmail.com>
2024-05-06 16:03:01 +0530

    modify

--- bubble_short.py
+++ bubble_short.py
@@ -3,6 +3,10 @@ def bubbleSort(arr):
     for k in range(0, n):
         for j in range(0, n-k-1):
             if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
 arr =[12,6,7,8,10,2,9]
 
-print("Before")
+print("Before sorting: {}".format(arr))
+bubbleSort(arr)
+print("After sorting : {}".format(arr))
