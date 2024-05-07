ad3f31442de3b3e7c6ea63b6419cff7a778b216c
jahanRazh <jahanrazh@gmail.com>
2024-05-07 10:44:56 +0530

    modify

--- selection_short.py
+++ selection_short.py
@@ -1,15 +1,16 @@
 def selectionshort(arr):
     n = len(arr)
     for i in range(0,n):
-        min = i
-        for j in range(1,n):
-            if arr[min] > arr[j]:
-                min = j
+        min_index = i
+        for j in range(i+1,n):
+            if arr[min_index] > arr[j]:
+                min_index = j
 
-        arr[i],arr[min] = arr[min],arr[i]
+        arr[i],arr[min_index] = arr[min_index],arr[i]#swap karnav
 
 
 arr =[12,6,7,8,10,2,12,9]
+print("Before sorting :{}".format(arr))
 selectionshort(arr)
-print(arr)
+print("After sorting :",arr)
 
